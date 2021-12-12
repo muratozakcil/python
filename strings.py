e79ac7c94d7ebd59e48f62b3596006e4e9136375
Murat Özakçil <murat@sanalyer.com>
2021-12-12 21:06:07 +0300

    strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+#Stringler
+
+name = "Murat "
+surname = "Özakçil "
+
+print(name + surname)
+print((name + surname) * 2)
+print(name * 3)
+
+text = input("veri giriniz")
+
+print(text)
+print(type(text))
